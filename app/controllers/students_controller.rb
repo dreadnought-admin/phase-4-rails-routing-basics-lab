2ceddf0705f30d02479ed4d4429e5556c1d22ebe
dom alexander <dom.alexan@gmail.com>
2023-01-12 11:53:14 -0700

    commit tree c23500c306904531f864259da03bd2f04460308d

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,16 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end 
+
+    def grades
+        grades = Student.all.order(grade: :DESC)
+        render json: grades
+    end 
+
+    def highest_grade
+        highest_grade = Student.all.order(grade: :DESC).first
+        render json: highest_grade
+    end 
+end
